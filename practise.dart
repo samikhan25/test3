2edb4f5eed101378059588755497cb733ef0a25a
samikhan25 <samikhanbhai78983@gmail.com>
2025-09-22 02:57:18 +0600

    Add new code

---
+++ practise.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+main(){
+  // int number =0;
+  //
+  //     if(number > 0){
+  //       print("number is positive");
+  //     }else if (number < 0) {
+  //       print("number is negative");
+  //     }else {
+  //       print("number is zero");
+  //     }
+
+      int marks = 30;
+      if ( marks >= 90){
+        print("A");
+      }else if ( marks >= 80){
+        print("B");
+      }else if (marks >= 70){
+        print("C");
+      }else if (marks >= 60){
+        print('D');
+      }else {
+        print('f');}
+
+
+}
